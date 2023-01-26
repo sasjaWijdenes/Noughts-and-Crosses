2017c37d515aaa5a00c62369146da6c2813a8632
sasjaWijdenes <saswij88@gmail.com>
2023-01-26 17:57:10 +0000

    adds player turns

--- noughts-and-crosses/src/components/BoardDisplay.jsx
+++ noughts-and-crosses/src/components/BoardDisplay.jsx
@@ -1,10 +1,24 @@
+import { useState } from "react"
 
 
-const BoardDisplay = ({ setIsGameOver, settings:  [isHumanP1, isHumanP2, size]}) => {
+const BoardDisplay = ({ setIsGameOver, settings: [isHumanP1, isHumanP2, size] }) => {
+    const [isP1Turn, setIsP1Turn] = useState(true)
+
+    const toggleTurn = () => setIsP1Turn(prevTurn => !prevTurn),
+        selectCell = () => {
+            toggleTurn()
+    }
     
     return (
-        <div className="board" style={{gridTemplateColumns: `repeat(${size}, 1fr)`}} >
-            {Array.from(Array(size), () => Array(size).fill( <div className="cell">Cell</div> ))}
+        <div className="board-container">
+            <section className="info-container">
+                <div className={isP1Turn? 'is-turn': ''}>Player One: {isHumanP1 ? 'Human' : 'Computer'}</div>
+                <h1 className="divider"> X       |      O </h1>
+                <div className={!isP1Turn? 'is-turn': ''}>Player Two: {isHumanP2? 'Human': 'Computer'}</div>
+            </section>
+            <div className="board" style={{gridTemplateColumns: `repeat(${size}, 1fr)`}} >
+                {Array.from(Array(size), () => Array(size).fill(<div className="cell" onClick={selectCell} ></div> ))}
+            </div>
         </div>
     )
 }
