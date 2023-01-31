bf45ad8ddaffa1f1e9f08a815e960d8836a610ea
sasjaWijdenes <saswij88@gmail.com>
2023-01-31 14:13:38 +0000

    refactors win checking functions to work with minMax function

--- noughts-and-crosses/src/components/BoardDisplay.jsx
+++ noughts-and-crosses/src/components/BoardDisplay.jsx
@@ -8,44 +8,40 @@ const BoardDisplay = ({ setWinner, settings: [isHumanP1, isHumanP2, size] }) => 
     const winCount = size;
     
     const toggleTurn = () => setIsP1Turn(prevTurn => !prevTurn)
-
     const clickCell = (e, position) => {    //Places a mark and checks for win condition
         const playerMark = isP1Turn ? 'X' : 'O';
         if (!cellArray[position[0]][position[1]]) { //Check if valid cell
-            e.target.classList = `cell ${isP1Turn ? 'X' : 'O'}` //Add mark
+            e.target.classList = `cell ${playerMark}` //Add mark
             setCellArray(prevArray => {                 //Update the cellArray
-                    const result = selectCell([...prevArray], position, playerMark) //check game state
-                    console.log(result)
-                    if(result.gameRes !== null) setWinner(result.gameRes)           //update winner condition
-                    return result.array;                                            //return updated array
+                    const {array, gameRes} = selectCell([...prevArray], position, playerMark) //check game state
+                    if(gameRes !== null) setWinner(gameRes)           //update winner condition
+                    return array;                                            //return updated array
                 });
                 toggleTurn()            //Switch Player
         } else alert('That Square is already taken.')
     }
     const selectCell = (array, position, playerMark) => {       //Returns a modified cell array and terminal game result ( {array, gameRes} )
-            array[position[0]][position[1]] = playerMark                    //Adding mark to array
+        array[position[0]][position[1]] = playerMark                    //Adding mark to array
             const gameRes = winCheck(array, position, playerMark)           //checking for win condition
             return {array, gameRes}
         }
     const winCheck = (cellArr, lastMove, playerMark) => {  //Checks for win condition and returns X | O | draw | null
         if (cellArr.every(row => !row.includes(null))) return 'draw'
         const directions = [[[-1, 0], [1, 0]], [[-1, 1], [1, -1]], [[0, 1], [0, -1]], [[1, 1], [-1, -1]]]
-        let winner = null
+        let winner = null;
         directions.forEach(line => {
-            let markCount = checkLine(cellArr, lastMove, line[0]) + checkLine(cellArr, lastMove, line[1], playerMark)
+            let markCount = checkLine(cellArr, lastMove, line[0], playerMark) + checkLine(cellArr, lastMove, line[1], playerMark)
             if (markCount - 1 >= winCount){
                 winner = playerMark;
-                console.log('winner should change', winner)
             }
         })
-        console.log({winner})
         return winner
     }
     const checkLine = (cellArr, thisCell, direction, playerMark) => {   //recursively checks each cell in a given direction
         let currRow = thisCell[0], currCol = thisCell[1], nextRow = currRow + direction[0], nextCol = currCol + direction[1]
         return !(cellArr[currRow][currCol] === (playerMark)) ? 0              //is current cell current players mark?
                 : (nextRow >= 0 && nextRow < size && nextCol >= 0 && nextCol < size)    //does next cell exist?
-                ? checkLine(cellArr, [nextRow, nextCol], direction) + 1                 //call on next cell in line
+                ? checkLine(cellArr, [nextRow, nextCol], direction, playerMark) + 1                 //call on next cell in line
                 : 1
     }
     
