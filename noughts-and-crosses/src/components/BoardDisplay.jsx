1832bed8c67d16794a34d8eb0b8f0a84b46d7147
sasjaWijdenes <saswij88@gmail.com>
2023-04-18 14:43:01 +0100

    refactored elements of selectCell into seperate functions for flexibility when making the minMax function

--- noughts-and-crosses/src/components/BoardDisplay.jsx
+++ noughts-and-crosses/src/components/BoardDisplay.jsx
@@ -8,28 +8,35 @@ const BoardDisplay = ({ setWinner, settings: [isHumanP1, isHumanP2, size] }) => 
         [isP1Turn, setIsP1Turn] = useState(true)
     
     const winCount = size;
-    
+    const directions = [[[-1, 0], [1, 0]], [[-1, 1], [1, -1]], [[0, 1], [0, -1]], [[1, 1], [-1, -1]]]
     const toggleTurn = () => setIsP1Turn(prevTurn => !prevTurn)
+    const isValidCell = (cellArray, position) => cellArray[position[0]][position[1]]
+    
     const selectCell = (e, position) => {
-            if (!cellArray[position[0]][position[1]]) { //Check if valid cell
+        if (isValidCell(cellArray, position)) alert('That Square is already taken.')
+        else {
                 e.target.classList = `cell ${isP1Turn ? 'X' : 'O'}` //Add mark
                 setCellArray(prevArray => {         //Update array
                     const newArray = [...prevArray];
                     newArray[position[0]][position[1]] = isP1Turn ? 'X' : 'O';
-                    winCheck(newArray, position)
+                    if(isWin(newArray, position)) setWinner(isP1Turn ? 'X' : 'O')
+                    if(isDraw(newArray)) setWinner('draw')
                     return newArray;
                 });
                 toggleTurn()            //Switch Player
-            } else alert('That Square is already taken.')
-        }
-    const winCheck = (cellArr, lastMove) => {  //Checks for win of current player or draw
-        if (cellArr.every(row => !row.includes(null))) setWinner('Draw')
-        const directions = [[[-1, 0], [1, 0]], [[-1, 1], [1, -1]], [[0, 1], [0, -1]], [[1, 1], [-1, -1]]]
+            }
+    }
+    
+    const isWin = (cellArr, lastMove) => {  //Checks for win of current player
+        let win = false
         directions.forEach(line => {
             let markCount = checkLine(cellArr, lastMove, line[0]) + checkLine(cellArr, lastMove, line[1])
-            if (markCount - 1 >= winCount) setWinner(isP1Turn? 'X': 'O')
+            if (markCount - 1 >= winCount) win = true
         })
+        return win
     }
+
+    const isDraw = (cellArr) => cellArr.every(row => !row.includes(null))   //Is board state a draw
     const checkLine = (cellArr, thisCell, direction) => {   //recursively checks each cell in a given direction
         let currRow = thisCell[0], currCol = thisCell[1], nextRow = currRow + direction[0], nextCol = currCol + direction[1]
         return !(cellArr[currRow][currCol] === (isP1Turn ? 'X' : 'O')) ? 0              //is current cell current players mark?
